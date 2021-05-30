4cb85c33a05313a64ec8c5a4d45d85805ceb17b7
stashpomichter <pomichterstash@gmail.com>
2021-05-30 13:46:47 -0700

    Commit added airtable key

--- functions/courses.js
+++ functions/courses.js
@@ -3,8 +3,23 @@ const getCourses = require('./helpers/getCourses');
 const createCourse = require('./helpers/createCourse');
 const deleteCourse = require('./helpers/deleteCourse');
 const updateCourse = require('./helpers/updateCourse');
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyopzwYFCbZWZujH'}).base('apppnu5g72hMbidzJ');
+
+base('Vendor').find('recPlUlO7mPk4EhQz', function(err, record) {
+    if (err) { console.error(err); return; }
+    console.log('Retrieved', record.id);
+});
+
+
 exports.handler = async (event) => {
     if (event.httpMethod === 'GET') {
+
+
+
+
+
         return await getCourses(event);
     } else if (event.httpMethod === 'POST') {
         return await createCourse(event);
